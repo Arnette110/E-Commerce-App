d3b9cb85c51450675749f2dec180cc437a4f8178
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-06-18 22:04:13 -0600

    added header with styles and links to home and shop page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/Header/Header.component'
 
 
 // const HatsPage = () => (
@@ -14,6 +15,7 @@ import ShopPage from './pages/shop/shop.component'
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={ShopPage} />
