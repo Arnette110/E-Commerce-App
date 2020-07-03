141c9bbf3e05417c314829fc89facefe55a44b1b
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-07-03 17:50:27 -0600

    removed withRouter from header

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, withRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../../assets/original.svg'
 
 import './Header.styles.scss'
