0d6d37dc15430cd49791e57150eeece515b9f4ae
Arnette110 <christopher.ryan.ellis@gmail.com>
2020-07-14 11:06:12 -0600

    implemented useSelector and useDispatch for state management with redux

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Switch } from 'react-router-dom'
+import React, { useEffect,  } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
+
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignupPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/Header/Header.component'
+
+import { setCurrentUser } from './redux/user/user.actions'
+
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null)
-
+  // const [currentUser, setCurrentUser] = useState(null)
+  
+  const dispatch = useDispatch()
   
 
   useEffect(()=>{
     document.title = 'My e-commerce app'
+    
     let unsubscribeFromAuth = null
     unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = createUserProfileDocument(userAuth)
         ;
         (await userRef).onSnapshot(snapShot => {
-          setCurrentUser(
+          dispatch(setCurrentUser(
             { ...snapShot, id: snapShot.id, ...snapShot.data() }
-          )
+          ))
         })
       }
+
     });
     
     return () => {
@@ -34,12 +44,9 @@ function App() {
   },[])
   
   // console.log(currentUser)
-
-  
-
   return (
     <div>
-      <Header currentUser={currentUser} />
+      <Header  />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route exact path='/shop' component={ShopPage} />
--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 import { ReactComponent as Logo } from '../../assets/original.svg'
 import { auth } from '../../firebase/firebase.utils'
 
 import './Header.styles.scss'
 
-const Header = ({ currentUser }) => {
+const Header = () => {
+
+  const currentUser = useSelector(state => state.user.currentUser)
+
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
